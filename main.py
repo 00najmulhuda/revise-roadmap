c0e76296223dfe0d60c7af3b3f60ff0d6294211c
Najmul Huda <nh5037480@gmail.com>
2026-06-08 13:38:07 +0530

    exam is over and start revision - FastAPi basics complete

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-
 @app.get("/")
-def home():
-    return {"msg": "Hello FastAPI"}
+def greet():
+    return{"msg":"Hello Najmul!"}
+
+@app.get("/about")
+def abouut_me():
+    return{"msg":"I am Najmul Huda a student of BTECH CSE at S.I.E.T"}
+
+@app.post("/my-info")
+def my_info():
+    return{"msg":"I am a AI Fullstack developer"}
+
+@app.put("/update-info")
+def update_info():
+    return{"msg":"I am AI fullstack developer - FastAPI,PostgreSQL,Python"}
+
+@app.delete("/delete-info")
+def delete_info():
+    return{"msg":"I deleted my info"}
+
 
 
